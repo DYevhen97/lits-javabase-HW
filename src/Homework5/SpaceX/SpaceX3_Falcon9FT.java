f48deb01385e7ce0161cf1440d65ab488909cb92
DYevhen97 <yevhendamrin@gmail.com>
2020-12-23 14:06:49 +0200

    homework 5

--- src/Homework5/SpaceX/SpaceX3_Falcon9FT.java
+++ src/Homework5/SpaceX/SpaceX3_Falcon9FT.java
@@ -2,10 +2,13 @@ package Homework5.SpaceX;
 
 public class SpaceX3_Falcon9FT extends SpaceX2_Falcon9_1_1 {
 
-    public int TotalLaunches;
+    private int TotalLaunches;
 
     public SpaceX3_Falcon9FT(int year, int weight, double length, double diameter, boolean status, int TotalLaunches) {
         super (year, weight, length, diameter, status);
+        this.setTotalLaunches(TotalLaunches);
+    }
+    public void setTotalLaunches(int totalLaunches) {
         this.TotalLaunches = TotalLaunches;
     }
 }
