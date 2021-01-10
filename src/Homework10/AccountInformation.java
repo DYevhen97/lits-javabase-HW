31481eeab0be6ab8f2404b9df709e7e88743c9be
DYevhen97 <yevhendamrin@gmail.com>
2021-01-10 16:11:55 +0200

    HomeWork 10 (Unfinished)

---
+++ src/Homework10/AccountInformation.java
@@ -0,0 +1,25 @@
+package Homework10;
+
+public class AccountInformation {
+
+    public String name;
+    public String surname;
+    public int age;
+    public String position;
+    public String phoneNumber;
+    public String email;
+    public String address;
+    public String role;
+
+    public AccountInformation (String name, String surname, int age, String position, String phoneNumber, String email, String address, String role) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.role = role;
+    }
+
+}
